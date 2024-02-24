8559fa869f5172da61722b9aedabfc3c2ef29889
Eric <270014@hkis.edu.hk>
2024-02-24 14:37:54 +0800

    hi

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 ##################################################
 '''
 Q1: 
+"It", "was", "a", "stormy", "night"
+
+list = ['It', 'was', 'a', 'stormy', 'night']
+
 '''
 
 # TODO: Write your code here
@@ -8,6 +12,9 @@ Q1:
 ##################################################
 '''
 Q2:
+
+list.insert(3, 'dark')
+list.insert(4, 'and')
 '''
 
 # TODO: Write your code here
@@ -15,6 +22,11 @@ Q2:
 ##################################################
 '''
 Q3:
+
+   ["It", "IS", "a", "dark", "and", "stormy", "night"]
+
+list[1] = 'IS'
+ 
 '''
 
 # TODO: Write your code here
@@ -22,20 +34,44 @@ Q3:
 ##################################################
 '''
 Q4:
+for word in list:
+    if 'a' in word:
+        list.remove(word)
+
+
+for word in list(len(line)-1, 0, -1):
+    word = line[i]
+    if 'a' in word:
+        line.remove(word)
+print line
+
 '''
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q5:
+
+evens = []
+for i in range(0, 20, 2):
+    print(f'i is {i}')
+    evens.append(i)
+
+
 '''
+
+
+
 
 # TODO: Write your code here
 
 ##################################################
 '''
 Q6:
+
+
 '''
 
 # TODO: Write your code here
